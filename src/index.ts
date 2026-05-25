c9d0c66976ab58c3895e1619889fd0979a5396a6
kyfex-uwu <kyfexuwu@gmail.com>
2026-05-25 14:06:46 -0600

    oops

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import {ref, Ref} from "./utils.js";
+import {ref, Ref, computed, Computed} from "./utils.js";
 import Router, {PageAttachedRouter} from "./Router.js";
 import createGenerator, {ArrowElementGenerator} from "./ArrowElementGenerator.js";
 
 export {
-    ref, Ref,
+    ref, Ref, computed, Computed,
     Router, PageAttachedRouter,
     createGenerator, ArrowElementGenerator
 }
